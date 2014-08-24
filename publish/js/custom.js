1bc241043120c5e47707c476d25581ecbfefd1a2
Nguyen Thanh Trung <ngthanhtrung23@gmail.com>
2014-08-24 11:05:39 +0800

    Add back button

--- publish/js/custom.js
+++ publish/js/custom.js
@@ -18,6 +18,12 @@ $(document).ready(function () {
             $('#' + new_id + "-toggle").parent().toggleClass("active");
         });
     })
+    if ( ($(window).height() + 100) < $(document).height() ) {
+        $('#top-link-block').removeClass('hidden').affix({
+            // how far to scroll down before link "slides" into view
+            offset: {top:100}
+        });
+    }
 });
 
 function load_user_rank() {
